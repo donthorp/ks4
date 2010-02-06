44fae049818d1b65ae1d88a6adefe1bdbe2c850c
donthorp <don@donthorp.net>
2010-02-06 14:25:37 -0600

    Project files

---
+++ src/org/appcelerator/kitchensink/KitchenSinkActivity.java
@@ -0,0 +1,13 @@
+package org.appcelerator.kitchensink;
+
+import org.appcelerator.titanium.TiRootActivity;
+
+import android.os.Bundle;
+
+public class KitchenSinkActivity extends TiRootActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/org/appcelerator/kitchensink/KitchenSinkApplication.java
@@ -0,0 +1,9 @@
+package org.appcelerator.kitchensink;
+
+import org.appcelerator.titanium.TiApplication;
+
+public class KitchenSinkApplication extends TiApplication {
+
+	public KitchenSinkApplication() {
+	}
+}
