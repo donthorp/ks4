89c56628d1103ba008bb40fcf074d6579bda6292
marshall <mculpepper@appcelerator.com>
2010-09-13 10:27:40 -0500

    changes for the new kroll refactor

--- src/org/appcelerator/kitchensink/KitchenSinkActivity.java
+++ src/org/appcelerator/kitchensink/KitchenSinkActivity.java
@@ -2,12 +2,6 @@ package org.appcelerator.kitchensink;
 
 import org.appcelerator.titanium.TiRootActivity;
 
-import android.os.Bundle;
-
-public class KitchenSinkActivity extends TiRootActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class KitchensinkActivity extends TiRootActivity
+{
 }
--- src/org/appcelerator/kitchensink/KitchenSinkAppInfo.java
+++ src/org/appcelerator/kitchensink/KitchenSinkAppInfo.java
@@ -5,58 +5,70 @@ import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.titanium.TiProperties;
 import org.appcelerator.titanium.util.Log;
 
-public class KitchenSinkAppInfo implements ITiAppInfo
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public class KitchensinkAppInfo implements ITiAppInfo
 {
 	private static final String LCAT = "AppInfo";
-
-	public KitchenSinkAppInfo(TiApplication app) {
-		TiProperties properties = app.getAppProperties();
-
-		properties.setBool("ti.android.debug", true);
-
-		// don
-		//properties.setString("ti.android.google.map.api.key", "0Rq5tT4bUSXcVQ3F0gt8ekVBkqgn05ZJBQMj6uw");					
-		// marshall
-		properties.setString("ti.android.google.map.api.key.development", "0A5J3AcoQizmxgmS-SMUm4JttfeX-ugVYcW7mew");
+	
+	
+	public KitchensinkAppInfo(TiApplication app) {
+		TiProperties properties = app.getSystemProperties();
+					
+		properties.setBool("ti.android.debug", false);
+					
+		properties.setString("ti.deploytype", "development");
+					
+		properties.setString("ti.android.google.map.api.key", "0ZnKXkWA2dIAu2EM-OV4ZD2lJY3sEWE5TSgjJNg");
 	}
-
+	
 	public String getId() {
-		return "com.nolanwright.kitchensink";
+		return "com.nolanwright.kitchensink2";
 	}
-
+	
 	public String getName() {
 		return "KitchenSink";
 	}
-
+	
 	public String getVersion() {
 		return "1.0";
 	}
-
+	
 	public String getPublisher() {
 		return "nwright";
 	}
-
+	
 	public String getUrl() {
 		return "appcelerator.com";
 	}
-
+	
 	public String getCopyright() {
-		return "2010 by nwright";
+		return "2009 by nwright";
 	}
-
+	
 	public String getDescription() {
 		return "No description provided";
 	}
-
+	
 	public String getIcon() {
 		return "default_app_logo.png";
 	}
-
+	
 	public boolean isAnalyticsEnabled() {
 		return true;
 	}
-
+	
 	public String getGUID() {
 		return "6fe33f33fd1f4e95a06d2d217170866d";
+	}
+	
+	public boolean isFullscreen() {
+		return false;
+	}
+	
+	public boolean isNavBarHidden() {
+		return false;
 	}
 }
